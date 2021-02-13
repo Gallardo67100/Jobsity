8b360c29b4503e61d9ce4aa7968e0ea8fe4f2d89
Facundo Gallardo <fgallardo@grupoprominente.com>
2021-02-13 16:57:43 -0300

    Created chatbot project, added RabbitMQ dependency on both projects

---
+++ JobsityChatroom.Bot/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Configuration;
+
+namespace JobsityChatroom.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = ConfigurationManager.AppSettings["RabbitMQHost"] };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: ConfigurationManager.AppSettings["RabbitMQQueueName"],
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    
+                    // Send Message
+                };
+                channel.BasicConsume(queue: ConfigurationManager.AppSettings["RabbitMQQueueName"],
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
