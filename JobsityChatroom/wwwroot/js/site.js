11c3a8c6c3293c3c94a8b134e204af9e29da3551
Facundo Gallardo <fgallardo@grupoprominente.com>
2021-02-13 16:14:46 -0300

    Clear input when a message is sent, fixed user name display on messages

--- JobsityChatroom/wwwroot/js/site.js
+++ JobsityChatroom/wwwroot/js/site.js
@@ -8,10 +8,11 @@ $("#sendButton").disabled = true;
 connection.on("ReceiveMessage", function (user, message, timestamp) {
 
     var newMessageClass = window.currentUser === user ? "ml-auto mr-2" : "mr-auto ml-2";
+    var newMessageUserName = window.currentUser === user ? "You" : user;
 
     // Creates a nes message DOM object and append it to the chat
     var newMessageDiv = "<div class='mt-2 border border-primary rounded bg-light pl-4 pr-4 pt-1 pb-1 " + newMessageClass + "'>";
-    newMessageDiv += "<small class='d-block text-left w-100 text-right m-0'>" + user + "</small>";
+    newMessageDiv += "<small class='d-block text-left w-100 text-right m-0'>" + newMessageUserName + " said: </small>";
     newMessageDiv += "<p class='text-left font-weight-bold'>" + message + "</p>";
     newMessageDiv += "<small class='d-block w-100 text-right m-0'>" + moment(timestamp).format("YYYY-MM-DD HH:mm:ss") + "</small>";
     newMessageDiv += "</div>";
